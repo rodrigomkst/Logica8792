fa699a11110ebd9a34c4ee5e197c18f6ae27a4a9
rodrigomkst <rodrigomkst@gmail.com>
2026-09-11 20:00:47 -0300

    Idade

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include<stdio.h> 
-#include<locale.h>
+#include<locale.h> 
 #include<stdbool.h>
 #include<windows.h>
+
 bool ehPar(int numero){ 
     return(numero % 2 == 0);
 }
@@ -11,13 +12,13 @@ int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8"); 
   
- int n; 
- 
- printf("digite um número"); 
- scanf("%d", &n); 
+ int idade; 
 
- printf ("Antecessor: %d\n",n - 1);
- printf ("sucessor: %d\n",n + 1);
+ printf("Digite a idade;"); 
+ scanf("%d", &idade); 
+
+ printf("Equivale a %d meses \n", idade * 12); 
+ printf("Equivale a %d dias (aproximado)\n", idade * 365);
  
    return 0; 
 }
