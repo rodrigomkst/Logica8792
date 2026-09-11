2ccc8543474d12f1d4df2127d2b2c132e7da22d1
rodrigomkst <rodrigomkst@gmail.com>
2026-09-11 19:39:01 -0300

    sucessor e antecessor

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include<stdio.h> 
 #include<locale.h>
 #include<stdbool.h>
-
+#include<windows.h>
 bool ehPar(int numero){ 
     return(numero % 2 == 0);
 }
@@ -11,19 +11,13 @@ int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8"); 
   
-  bool resultado1 = ehPar(4);
-  bool resultado2 = ehPar(7); 
-  bool resultado3 = ehPar(8);
-  bool resultado4 = ehPar(5); 
-  bool resultado5 = ehPar(2);
-
-
-  printf("4 é par? %d\n", resultado1);
-  printf ("7 é par? %d\n",resultado2); 
-  printf("8 é par? %d\n",resultado3);
-  printf("5 é par? %d\n",resultado4); 
-  printf("2 é par? %d\n",resultado5);
+ int n; 
  
+ printf("digite um número"); 
+ scanf("%d", &n); 
+
+ printf ("Antecessor: %d\n",n - 1);
+ printf ("sucessor: %d\n",n + 1);
  
    return 0; 
 }
