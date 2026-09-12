5cbbbe51913f254536a88ec80d0e98c8b496d095
rodrigomkst <rodrigomkst@gmail.com>
2026-09-11 21:34:54 -0300

    temperatura nao funcionou direito

--- main.c
+++ main.c
@@ -3,21 +3,15 @@
 #include<stdbool.h>
 #include<windows.h>
 
-bool ehPar(int numero){ 
-    return(numero % 2 == 0);
-}
-
-
 int main(){   
 
 
-float n1, n2, n3, media; 
-
-printf("Digite tres notas: "); 
-scanf("%f %f %f", &n1, &n2, &n3); 
-media = (n1 + n2 + n3) / 3;
-
-printf("Medias: %2.f\n", media);
  
+ printf ("Digite a temperatura em celsius: "); 
+ scanf("%f", &celsius); 
+
+ fahrenheit = (celsius * 9.0 / 5.0) + 32; 
+ printf ("Fahrenheit: %.2f\n", fahrenheit); 
+
    return 0; 
 }
