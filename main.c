eeb443df57827b2d600a6f81af63f7bf351562b4
rodrigomkst <rodrigomkst@gmail.com>
2026-09-14 21:35:43 -0300

    maior e menor

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8"); 
   
-int a, b, c, d, e, menor; 
+int a, b, c, d, e, menor, maior; 
 
 printf("digite um numero para a:");
 scanf("%d", &a); 
@@ -17,21 +17,28 @@ scanf("%d", &b);
 printf("digite um valor para c: "); 
 scanf("%d", &c); 
 
+printf("digite um valor para d: "); 
+scanf("%d", &d);
+
 printf("digite um valor para e: "); 
 scanf("%d", &e);
   
 menor = a; 
 
-if(b > menor) menor =b; 
-if(c > menor) menor =c; 
-if(d > menor) menor =d;
-if(e > menor) menor =e; 
+if(b < menor) menor = b; 
+if(c < menor) menor = c; 
+if(d < menor) menor = d;
+if(e < menor) menor = e; 
 
+maior = a;
 
-printf("menor: %d", menor); 
+ if(b > maior) maior = b; 
+ if(c > maior) maior = c; 
+ if(d > maior) maior = d; 
+ if(e > maior) maior = e; 
 
- 
-
+ printf("maior: %d\n", maior);
+ printf("menor: %d\n", menor); 
 
 
    return 0;
