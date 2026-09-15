99b8992e427f34e7831b39f55a0815c9f3547dfa
rodrigomkst <rodrigomkst@gmail.com>
2026-09-14 21:43:32 -0300

    comparação de numeros

--- main.c
+++ main.c
@@ -6,39 +6,17 @@ int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8"); 
   
-int a, b, c, d, e, menor, maior; 
+int a, b; 
 
-printf("digite um numero para a:");
-scanf("%d", &a); 
+printf("digite dois numeros: ");
+scanf("%d %d", &a, &b); 
 
-printf("digite um numero para b: ");
-scanf("%d", &b); 
+if(a == b){
+  printf("os numeros sao iguais!\n"); 
+}else{
+  printf("os numeros sao diferentes!\n");
+}
 
-printf("digite um valor para c: "); 
-scanf("%d", &c); 
-
-printf("digite um valor para d: "); 
-scanf("%d", &d);
-
-printf("digite um valor para e: "); 
-scanf("%d", &e);
-  
-menor = a; 
-
-if(b < menor) menor = b; 
-if(c < menor) menor = c; 
-if(d < menor) menor = d;
-if(e < menor) menor = e; 
-
-maior = a;
-
- if(b > maior) maior = b; 
- if(c > maior) maior = c; 
- if(d > maior) maior = d; 
- if(e > maior) maior = e; 
-
- printf("maior: %d\n", maior);
- printf("menor: %d\n", menor); 
 
 
    return 0;
