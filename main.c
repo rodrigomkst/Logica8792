e4a03ed9f8c6709fb587c1230070cdcefe21410f
rodrigomkst <rodrigomkst@gmail.com>
2026-09-18 20:20:52 -0300

    estaçoes gabarito do sor

--- main.c
+++ main.c
@@ -9,62 +9,32 @@ setlocale(LC_ALL, "pt_BR.UTF-8");
 
  int mes; 
 
- printf("digite um mes de 1 a 12");
- printf("1 - janeiro\n");
- printf("2 - fevereiro\n"); 
- printf("3 - março\n");
- printf("4 - abril\n"); 
- printf("5 - maio");
- printf("6 - junho\n");
- printf("7 - julho\n");
- printf("8 - agosto\n");
- printf("9  - setembro\n");
- printf("10 - outubro\n");
- printf("11 - novembro\n");
- printf("12 - dezembro\n"); 
-  scanf("%d", &mes); 
+ printf("digite um valor de 1 a 12\n");
+ scanf("%d", &mes); 
 
-  switch (mes) 
-  {
-    case 1:
-      printf("janeiro | verão");
-      break;
-    case 2:
-      printf("fevereiro | verão");
-      break;
-    case 3: 
-      printf("março | verão");
-      break;
-    case 4:
-      printf("abril | outono");
-      break;
-    case 5:
-      printf("maio | outono");
-      break;
-    case 6:
-      printf("junho | outono");
-      break;
-    case 7: 
-      printf("julho | outono");
-      break;
-    case 8:
-      printf("agosto | inverno");
-      break;
-    case 9:
-      printf("setembro | inverno"); 
-      break; 
-    case 10: 
-      printf("outubro | inverno");
-      break;
-    case 11: 
-      printf("novembro | primavera");
-      break;
-    case 12:
-      printf("dezembro | primavera");
-      break;
-      
-  }
-
+ switch (mes) 
+ {
+  case 12:
+  case 1:
+  case 2:
+    printf("verão\n");
+  break;
+  case 3:
+  case 4:
+  case 5:
+    printf("outono\n");
+    break;
+  case 6: 
+  case 7:
+  case 8:
+    printf("inverno\n");
+    break;
+  case 9: 
+  case 10:
+  case 11:
+    printf("primavera\n");
+    break;
+ }
 
       
    return 0;
