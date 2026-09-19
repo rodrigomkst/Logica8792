93321f00de5bf4eb830709434e0e346a93015135
rodrigomkst <rodrigomkst@gmail.com>
2026-09-18 21:04:55 -0300

    multiplo

--- main.c
+++ main.c
@@ -6,14 +6,20 @@ int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8"); 
   
-char letra;
-printf("digite uma letra: ");
-scanf("%c", &letra);
 
-if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' || letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U' ){
-  printf("vogal\n");
+
+int n; 
+printf("Digite um numero:");
+scanf("%d", &n); 
+
+if(n % 3 == 0 && n % 5 ==0){
+  printf("Multiplo de 3 e 5\n");
+}else if(n % 3 ==0){
+  printf("Multiplo de 3\n");
+}else if(n % 5 == 0){
+  printf("Multiplo de 5\n");
 }else{
-  printf("consoante\n");
+  printf("Não e multiplo de 3 nem de 5\n");
 }
  
 
