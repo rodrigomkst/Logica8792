6bde6e3488a31f14e029ff05044728da8573cc7e
rodrigomkst <rodrigomkst@gmail.com>
2026-09-23 20:12:09 -0300

    tabuada com int

--- main.c
+++ main.c
@@ -3,20 +3,23 @@
 #include<windows.h>
 #include<math.h>
 
-void tabuada (int n){
-    printf("tabuada do %d\n", n);
-    for(int i = 1; i <=10; i++){
-        printf("%d x %d = %d\n", n, i, n* i); 
+int tabuada (int n){
+    int resultado;
+    printf("tabuada do %d:\n", n);
+    for(int i = 1; i <= 10; i++){
+        resultado = n * i;
+        printf("%d x %d = %d\n", n, i, resultado);
     }
+   return resultado;
 }
-
 
 int main(){ 
 
 setlocale(LC_ALL, "pt_BR.UTF-8"); 
-
- tabuada(7);
-
+    int ultimo = tabuada(7);
+    printf("ultima valor da tabuada: %d\n", ultimo); 
+    
+ 
  
    return 0;
 }
