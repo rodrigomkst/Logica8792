677bcdaea56d10db2f414c0b80cc3c906715fba2
rodrigomkst <rodrigomkst@gmail.com>
2026-09-23 19:09:49 -0300

    ola

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
 #include<stdio.h> 
 #include<locale.h> 
 #include<windows.h>
+#include<string.h>
+#include<math.h>
 
 int main(){ 
 
 setlocale(LC_ALL, "pt_BR.UTF-8"); 
   
+  char none[50];
+  
+  printf("Digite seu nome completo aqui ");
+  fgets(none, sizeof(none), stdin);
 
-for(int i = i; i <= 100; i++){
-    printf("%d\n", i);
-}
-      
+printf("ola %s", none);
+
    return 0;
 }
 
