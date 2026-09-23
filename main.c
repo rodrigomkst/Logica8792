f28eb2d3da3b799f7f01e63acf9acc3760f3ce52
rodrigomkst <rodrigomkst@gmail.com>
2026-09-23 19:19:36 -0300

    fatorial

--- main.c
+++ main.c
@@ -1,20 +1,22 @@
 #include<stdio.h> 
 #include<locale.h> 
 #include<windows.h>
-#include<string.h>
 #include<math.h>
+
+int fatorial(int n){
+    int resultado = 1; 
+    for(int i = 1; i <= n; i++){
+        resultado *= i;
+    }
+     return resultado;
+}
 
 int main(){ 
 
 setlocale(LC_ALL, "pt_BR.UTF-8"); 
-  
-  char none[50];
-  
-  printf("Digite seu nome completo aqui ");
-  fgets(none, sizeof(none), stdin);
-
-printf("ola %s", none);
-
+int numero = 5; 
+printf("fatorial de %d = %d\n", numero, fatorial(numero));  
+ 
    return 0;
 }
 
