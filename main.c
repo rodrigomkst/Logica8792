4ba2583c71bb46cb8f5cdc69fc04bd71f3bb58ca
rodrigomkst <rodrigomkst@gmail.com>
2026-09-24 19:10:47 -0300

    while

--- main.c
+++ main.c
@@ -4,22 +4,18 @@
 #include<math.h>
 #include<string.h>
 
-void maxMin(int v [], int tamanho){
-    int max = v[0], min = v[0];
-    for(int i = 1; i < tamanho; i++){
-        if(v[i] > max) max = v[i];
-        if(v[i] < min) min = v[i];
-    }
-    printf("Maximo: %d, minimo: %d\n", max, min);
+int main(){
+setlocale(LC_ALL, "pt_BR.UTF-8"); 
+
+int i = 5; 
+
+while(i <= 6){
+   printf("%d\n", i);
+   i++; 
 }
 
-    
-int main(){ 
 
-setlocale(LC_ALL, "pt_BR.UTF-8"); 
-  
-int numeros[] = {4, 8, 2, 15, 6};
-maxMin(numeros, 5);
+
    return 0;
 }
 
