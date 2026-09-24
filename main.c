7bafa48a26eb0301a3f9bbc46773304724f8e5cc
rodrigomkst <rodrigomkst@gmail.com>
2026-09-24 19:42:27 -0300

    sistema de atm

--- main.c
+++ main.c
@@ -7,18 +7,25 @@
 int main(){
 setlocale(LC_ALL, "pt_BR.UTF-8"); 
 
-int numero;
-printf("Digite um numero positivo: ");
-scanf("%d", numero);
+int opcao = -1;
 
-while(numero <= 0){
-   printf("Numero invalido! digite novamente: ");
-   scanf("%d", &numero);
+while(opcao != 0){
+   printf("\nMenu: \n");
+   printf("1 - Depositar\n");
+   printf("2 - sacar\n");
+   printf("3 - consultar\n");
+   printf("0 - sair\n");
+   printf("Escolha: ");
+   scanf("%d", &opcao); 
 
+   switch(opcao){
+      case 1: printf("Voce escolheu depositar\n"); break;
+      case 2: printf("Voce escolheu sacar\n"); break;
+      case 3: printf("Voce escolheu consultar\n"); break;
+      case 0: printf("Saindo..."); break;
+      default: printf("Opção invalida!\n");
+   }
 }
-
-printf("Numero valido: %d\n", numero);
-
    return 0;
 }
 
