c510643f30be8276c1406a3b3a44419d8d933ca5
rodrigomkst <rodrigomkst@gmail.com>
2026-09-24 20:33:38 -0300

    contagem de numeros pares com while

--- main.c
+++ main.c
@@ -7,17 +7,13 @@
 int main(){
 setlocale(LC_ALL, "pt_BR.UTF-8"); 
 
-int numero, soma = 0;
-
-printf("Digite numero para somar (0 encerra): ");
-scanf("%d", &numero);
-
-while(numero != 0){
-   soma += numero;
-   scanf("%d", &numero);
-
+int i = 1;
+while(i <= 50){
+   if(i % 2 == 0)
+  printf("%d\n", i);
+   
 }
-printf("Soma total = %d\n", soma);
+ i++;  
    return 0;
 }
 
