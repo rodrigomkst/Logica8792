c8ebd1cc22c333103e5f8418e06e0f31f56a1a1c
rodrigomkst <rodrigomkst@gmail.com>
2026-09-25 20:06:22 -0300

    Numero perfeito

--- main.c
+++ main.c
@@ -4,30 +4,25 @@
 #include<math.h>
 #include<string.h>
 
-int ehPrimo(int n){
-   if(n < 2)return 0;
-   for(int i = 2; i < n; i++){
-      if(n % i == 0) return 0;
-   }
-return 1;
-
-
-
-}
-
 
 int main(){
 setlocale(LC_ALL, "pt_BR.UTF-8"); 
 
-int numero;
+int n, soma = 0;
 
 printf("Digite um numero: ");
-scanf("%d", &numero);
-if(ehPrimo(numero))
-      printf("%d é primo\n", numero);
-else 
-   printf("%d Não é primo\n", numero);
+scanf("%d", &n);
 
+for(int i = 1; i < n; i++){
+   if(n % i == 0){
+      soma += i;
+   }
+}
+if(soma == n){
+   printf("%d é um numero perfeito\n", n);
+}else{
+   printf("%d Não é um numero perfeito\n", n);
+}
 
 
    return 0;
