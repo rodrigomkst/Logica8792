d55538a21e8bdacac61aff31e6a320aa8a8fc4a7
rodrigomkst <rodrigomkst@gmail.com>
2026-09-25 19:48:46 -0300

    tambem numero primo

--- main.c
+++ main.c
@@ -4,29 +4,30 @@
 #include<math.h>
 #include<string.h>
 
+int ehPrimo(int n){
+   if(n < 2)return 0;
+   for(int i = 2; i < n; i++){
+      if(n % i == 0) return 0;
+   }
+return 1;
+
+
+
+}
+
+
 int main(){
 setlocale(LC_ALL, "pt_BR.UTF-8"); 
 
-int n, primo = 1;
+int numero;
 
-printf("Digite seu numero: ");
-scanf("%d", &n);
+printf("Digite um numero: ");
+scanf("%d", &numero);
+if(ehPrimo(numero))
+      printf("%d é primo\n", numero);
+else 
+   printf("%d Não é primo\n", numero);
 
-if(n < 2){
-   primo = 0;
-}else{
-   for(int i = 2; i <= n / 2; i++){
-      if(n % i ==0){
-         primo = 0;
-         break;
-      }
-   }
-}
-if(primo){
-   printf("%d é primo\n");
-}else{
-   printf("%d não é primo\n", n);
-}
 
 
    return 0;
