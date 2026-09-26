d917da8d58fb40393a14b5d9c206296a803ab39d
rodrigomkst <rodrigomkst@gmail.com>
2026-09-25 21:51:07 -0300

    Calculo tudo junto

--- main.c
+++ main.c
@@ -8,18 +8,58 @@
 int main(){
 setlocale(LC_ALL, "pt_BR.UTF-8"); 
 
-int n;
-float nota, soma = 0;
+int o;
+float a, b;
 
-printf("Quantos alunos tem a turma: ");
-scanf("%d", &n);
+do{
+   printf("\n---MENU---\n");
+   printf("1 - soma\n");
+   printf("3 - multiplicação\n");
+   printf("4 - divisão\n");
+   printf("0 - sair\n");
+   printf("Escolha uma opção");
+   scanf("%d", &o);
 
-for(int i = 0; i < n; i++){
-   printf("Digite a nota do aluno %d: \n", i + 1);
-   scanf("%f", &nota);
-   soma += nota;
-}
-printf("Media da turma: %.2f\n", soma / n);
+   switch(o){
+      
+      case 1:
+         printf("Digite dois numeros: ");
+         scanf("%f %f", &a, &b);
+         printf("Resultado: %.f\n", a + b);
+         break;
+      
+      case 2:printf("Digite dois numeros: ");
+             scanf("%f %f", &a, &b);
+             printf("Resultado: %.f\n", a - b);
+             break;
+    
+      case 3: 
+         printf("Digite dois numeros: ");
+         scanf("%f %f", &a, &b);
+         printf("Resultado: %.2f\n", a * b);
+         break;
+     
+      case 4:
+         printf("Digite dois numeros: ");
+         scanf("%f %f", &a, &b);
+         if(b != 0)
+            printf("Resultado: %.2f\n", a / b);
+         else 
+            printf("Erro: divisão por zero!\n");
+         break;
+      
+      case 0:
+         printf("Saindo...");
+         break;
+      
+      default: 
+         printf("opção invalida!\n");
+         break;
+
+
+            
+   }
+}while(o != 0);
    return 0;
 }
 
